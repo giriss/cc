14fb7108ce560e566da0b1ceca89f11ec1ecdc05
Girish Gopaul <akhile@dr.com>
2013-10-26 07:18:31 +0000

    Update views.py

--- users/views.py
+++ users/views.py
@@ -41,7 +41,7 @@ def register(request):
         user_add = user(fullName=fn, email=em, password=pw, dateJoined=dt)
         user_add.save()
         request.session['userId'] = user_add.id
-        request.session['userName'] = user_add.fullName
+        request.session['userName'] = fn
         return HttpResponseRedirect(reverse('home'))
 
 
