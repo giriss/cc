c8668b11e35368cc0d8a91744f4e4806ab4e907a
Girish Gopaul <akhile@dr.com>
2013-10-26 07:58:36 +0000

    Update views.py

--- tutize/views.py
+++ tutize/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 
 
@@ -10,3 +10,8 @@ def home(request):
         return render(request, "home/index.html")
     else:
         return HttpResponseRedirect(reverse("u:home"))
+
+
+def sslverify(request):
+    return HttpResponse("""31DCCBD89DC9A38C468754142ECBBBADB8D8DB68
+comodoca.com""")
